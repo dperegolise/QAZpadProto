b3b5f2fe31816f339640cf0e09f5b61e89127ca0
dperegolise <danielperegolise@gmail.com>
2021-06-21 15:57:30 -0400

    fixing keymap for southpaw

--- keymaps/default/keymap.c
+++ keymaps/default/keymap.c
@@ -16,15 +16,26 @@
 
 #include QMK_KEYBOARD_H
 
+enum layer_names {
+     _BASE, 
+     _MODS
+};
+
 #define LT1_BSP     LT(1, KC_BSPC)
 #define LT2_SPC     LT(2, KC_SPC)
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
-    [0] = LAYOUT(
-        KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,             KC_P7,   KC_P8,   KC_P9,   \
-        KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_ENT,           KC_P4,   KC_P5,   KC_P6,   \
-        KC_Z,    KC_NO,   KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_DOT,  KC_RSFT,          KC_P1,   KC_P2,   KC_P3,   \
-        KC_LCTL, KC_LGUI, KC_LALT, KC_BSPC, KC_ESC,  KC_TAB,  KC_CAPS, KC_SPC,  KC_UP,   KC_LEFT, KC_DOWN, KC_RGHT, KC_P0,   KC_PDOT \
+    [_BASE] = LAYOUT(
+        KC_P7,   KC_P8,   KC_P9,            KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,                \
+        KC_P4,   KC_P5,   KC_P6,            KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_ENT,              \
+        KC_P1,   KC_P2,   KC_P3,   KC_NO,   KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,             \
+        KC_P0,   KC_NO,   KC_DOT,  KC_LCTL, KC_LGUI, KC_LALT,  KC_NO,   MO(_MODS), KC_NO,   KC_SPC,  KC_NO,   KC_LEFT, KC_DOWN, KC_RGHT \
     ),
+    [_MODS] = LAYOUT(
+        KC_TRNS,   KC_TRNS,   KC_TRNS,            KC_ESC,    KC_TRNS,    KC_TRNS,    KC_TRNS,    KC_TRNS,    KC_TRNS,    KC_TRNS,    KC_TRNS,    KC_TRNS,    KC_BSPC,                \
+        KC_TRNS,   KC_TRNS,   KC_TRNS,            KC_TAB,    KC_TRNS,    KC_TRNS,    KC_TRNS,    KC_TRNS,    KC_TRNS,    KC_TRNS,    KC_TRNS,    KC_SCLN,    KC_QUOT,              \
+        KC_TRNS,   KC_TRNS,   KC_TRNS,  KC_TRNS,  KC_LSFT,   KC_TRNS,    KC_TRNS,    KC_TRNS,    KC_TRNS,    KC_TRNS,    KC_TRNS,    KC_TRNS,    KC_SLSH,             \
+        KC_TRNS,   KC_TRNS,   KC_TRNS,  KC_TRNS,  KC_TRNS,   KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,    KC_TRNS,    KC_TRNS,    KC_TRNS,    KC_TRNS \
+    )
 
 };
